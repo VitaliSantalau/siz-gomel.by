9d92aafa82acb313987d7f83416d1a6620bbf787
VitaliSantalau <v.santalau@gmail.com>
2020-12-09 17:44:19 +0300

    start create page

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,5 +1,15 @@
 /* https://www.gatsbyjs.com/docs/gatsby-config/ */
 
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: "./data/",
+      },
+    },
+    "gatsby-transformer-json",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+  ],
 }
--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -1,13 +1,1 @@
 
-exports.createPages = ({ actions: { createPage } }) => {
-  createPage({
-      path: "/sample/",
-      component: require.resolve("./src/templates/sample.js"),
-      context: {
-        name: "Ботинки",
-        price: "25.8",
-        content: "<h1>Ботинки</h1>",
-      }
-  })
-}
-
