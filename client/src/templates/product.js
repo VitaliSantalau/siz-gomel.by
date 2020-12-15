d3a22d1490b20c587d4b15f241441292e97bd2f5
VitaliSantalau <v.santalau@gmail.com>
2020-12-16 00:40:22 +0300

    create product first

--- client/src/templates/product.js
+++ client/src/templates/product.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
-import style from "../css/mainSection.module.css"
+import style from "../css/product.module.css"
 
 import MainLayout from "../components/mainLayout"
 import CatalogNav from "../components/catalogNav"
@@ -10,6 +10,7 @@ export const query = graphql`
   query($slug: String!) {
     productJson(slug: { eq: $slug }) {
       name
+      price
       image {
         childImageSharp {
           fluid(fit: COVER) {
@@ -18,6 +19,7 @@ export const query = graphql`
         }
       }
       alt
+      description
     }
   }
 `
@@ -27,17 +29,26 @@ const Product = ({ data }) => {
   return (
     <MainLayout>
       <main>
-      <CatalogNav />
-        <div className={style.containerGroupImage}>
-          <Img className={style.groupImage}
-            fluid={product.image.childImageSharp.fluid}
-            alt={product.alt}
-          />
-        </div>
-        <h2 className={style.groupName}>{product.name}</h2>
+        <CatalogNav />
+        <section className={style.product}>
+          <div className={style.containerImage}>
+            <Img className={style.imageProduct}
+              fluid={product.image.childImageSharp.fluid}
+              alt={product.alt}
+            />
+          </div>
+          
+          <div className={style.descriptionProduct}>
+            <h2 className={style.groupName}>{product.name}</h2>
+            <div dangerouslySetInnerHTML={{ __html: product.description }} className={style.text}/>
+            <div className={style.priceNds}>
+              <div className={style.price}>{product.price}</div>
+              <div className={style.nds}>(без НДС)</div>
+            </div>
+          </div>
+        </section>
       </main>
     </MainLayout>
   )
 }
-
 export default Product;
